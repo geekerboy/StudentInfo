0cd3d50d6b82efd1884b53391288271df1421b96
geekerboy <1632298575@qq.com>
2019-09-24 22:08:38 +0800

    Initial commit

---
+++ student.cpp
@@ -0,0 +1,22 @@
+
+#include "student.h"
+
+
+void Student::Average_score_single() {
+    int32_t average_score,sum;
+
+}
+
+void Student::Average_score_all() {
+
+}
+
+void Student::Max_score() {
+
+}
+
+void Output_info(Student stu) {
+
+}
+
+
---
+++ student.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class Student{
+public:
+    void Average_score_single();
+    void Average_score_all();
+    void Max_score();
+    friend void Output_info(Student stu);
+
+private:
+    int32_t  const student_id_[5]={1,2,3,4,5};
+    std::string  student_name_[5]={"zhao","qian","sun","li","zhou"};
+    int32_t  language_score_[5]={10,20,30,40,50};
+    int32_t  math_score_[5]={21,31,41,51,61};
+};
